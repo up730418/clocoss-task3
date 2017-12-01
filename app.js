97948650c0f3c34e27e3f81cdc7ba6829fdb1f84
up730418 <up730418@myport.ac.uk>
2017-12-01 12:14:08 +0000

    Added request and api/random forbiden  features

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const googleauth = require('simple-google-openid');
 const roles = ['admin','user'];
-let userRoles = [{"email": "up730418@myport.ac.ukzz", "roles": ['user']}, {"email": "up730418@myport.ac.uk", "roles": ['user','admin']},];
+let userRoles = [{"email": "up730418@myport.ac.ukzz", "roles": []}, {"email": "up730418@myport.ac.uk", "roles": ['user','admin']},];
 
 // you can put your client ID here
 app.use(googleauth('637021493194-nncq03bpm7am8odjsl69ibceoutch5k4.apps.googleusercontent.com'));
@@ -23,18 +23,28 @@ app.get('/api/protected', function (req, res) {
 });
 
 app.get('/api/random', function (req, res) {
-	res.send((Math.random()).toString());
+	let result = userRoles.find((user) => {return user.email == req.user.emails[0].value});
+console.log(result);
+	if(result.roles.includes("user") || result.roles.includes("admin")){
+	
+		res.send((Math.random()).toString());
+	} else {
+		res.sendStatus(403);
+	}
 });
 
 app.get('/api/user/roles', function (req, res) {
 	console.log(req.user.emails[0].value);
-	let result = userRoles.filter((user) => {return user.email == req.user.emails[0].value});
-	console.log(result[0].roles);
-	res.send(result[0].roles);
+	let result = userRoles.find((user) => {return user.email == req.user.emails[0].value});
+	console.log(result.roles);
+	res.send(result? result.roles : []);
 });
 
 app.get('/api/user/request', function (req, res) {
-
+	let result = userRoles.filter((user) => { return user.roles.length == 0 });
+	let userIds = [];
+	result.forEach((user) => { userIds.push(user.email) });
+	res.send(userIds);
 });
 
 app.post('/api/user/request', bodyParser.text(), function (req, res) {
